40e3f0b087b40923b6fb6a4c28c707e0d58db540
c_shunyi <c_shunyi@outlook.com>
2025-07-11 19:52:34 +0800

    添加跨域配置

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import indexRoutes from './routes/index.js';
 
 const app = express();
+
+// CORS 配置 - 允许所有来源
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: '*',
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 
 app.use('/api', indexRoutes);
